af160b922c9bb2ea5df69cd1c3bd2c3fe8c8b64b
Jay Pfaffman <pfaffman@gmail.com>
2021-03-14 11:40:22 -0700

    do not need routes

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-require_dependency "discourse_group_assign_by_custom_field_constraint"
-
-DiscourseGroupAssignByCustomField::Engine.routes.draw do
-  get "/" => "discourse_group_assign_by_custom_field#index", constraints: DiscourseGroupAssignByCustomFieldConstraint.new
-  get "/actions" => "actions#index", constraints: DiscourseGroupAssignByCustomFieldConstraint.new
-  get "/actions/:id" => "actions#show", constraints: DiscourseGroupAssignByCustomFieldConstraint.new
-end
